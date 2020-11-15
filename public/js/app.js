d9fc15191d4faa899447a177a7dd0979db771b9d
jungjh123456 <jungjh123456@naver.com>
2020-11-15 13:24:51 +0900

    fix: app.js bug remake

--- public/js/app.js
+++ public/js/app.js
@@ -18,12 +18,12 @@ $signupBtn.onclick = e => {
 
 // db
 
-function writeUserData(userId, name) {
+(function writeUserData(userId, name) {
   firebase.database().ref('users/' + userId ).set({
     username: name
   })
   
-}
+})();
 
 firebase.database().ref('users/').on('value', (snapshot) => {
   person = snapshot.val()

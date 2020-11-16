d3dfee448e5830e6f84667e50fe694bcc77b440a
jungjh123456 <jungjh123456@naver.com>
2020-11-16 09:46:41 +0900

    fix: js fixed

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,21 @@
+
+
 let perons = [];
-let database = firebase.database();
 
-const $signupFieldset = document.querySelector('.signup-fieldset');
-const $signupName = document.querySelector('.signup-name');
-const $signupNink = document.querySelector('.signup-nink');
-const $signupBtn = document.querySelector('.signup-btn');
-const $signupId = document.querySelector('.signup-id');
-const $input = document.querySelector('input');
+const $btnSignup = document.querySelector('.btn-signup');
 
-$signupBtn.onclick = e => {
-  e.preventDefault();
-  if ($input.value === '') console.log('비었습니다.');
-  writeUserData($signupId.value  , { name: $signupName.value, ninkname: $signupNink.value, id: $signupId.value })
+console.log('sdgds')
+
+$btnSignup.onclick = e => {
+  console.log('aas')
+  location.assign('../public/signup.html');
 }
-
 
 // db
 
 
 
-firebase.database().ref('users/').on('value', (snapshot) => {
-  person = snapshot.val()
-})
+
 
 
 // // storage
---
+++ public/js/sign.js
@@ -0,0 +1,18 @@
+let database = firebase.database();
+
+const $signupFieldset = document.querySelector('.signup-fieldset');
+const $signupName = document.querySelector('.signup-name');
+const $signupNink = document.querySelector('.signup-nink');
+const $signupBtn = document.querySelector('.signup-btn');
+const $signupId = document.querySelector('.signup-id');
+const $input = document.querySelector('input');
+
+$signupBtn.onclick = e => {
+  e.preventDefault();
+  if ($input.value === '') console.log('비었습니다.');
+  writeUserData($signupId.value  , { name: $signupName.value, ninkname: $signupNink.value, id: $signupId.value })
+}
+
+firebase.database().ref('users/').on('value', (snapshot) => {
+  person = snapshot.val()
+})

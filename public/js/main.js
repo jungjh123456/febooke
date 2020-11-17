bf070eb5ce1ecb811486f9c8c1398cd79b7e23da
jjm2317 <wlaks2317@gmail.com>
2020-11-17 19:43:58 +0900

    feat: upgrade mainPage

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,17 @@
 // --------------------------header 시계 -------------------------
 const $headerTimer = document.querySelector(".header-timer");
+const $logoTitle = document.querySelector('.logo-title');
+let title = [`<span class="wT">W</span>`,`<span>e</span>`,
+`<span> </span>`,`<span>a</span>`,`<span>r</span>`,
+`<span>e</span>`, `<span> </span>`,
+`<span class="logoT firstT">F</span>`,`<span>r</span>`,`<span>o</span>`,`<span>n</span>`,`<span>t</span>`, 
+`<span></span>`,`<span class="logoT secondT">E</span>`,
+`<span>n</span>`,`<span>d</span>`,`<span></span>`,
+`<span>D</span>`,`<span>e</span>`,`<span>v</span>`,`<span>e</span>`,`<span>l</span>`,`<span>o</span>`,
+`<span>p</span>`,`<span>e</span>`,`<span>r</span>`,`<span>s</span>`];
+
+let titleLength = title.length;
+const $logoImage = document.querySelector('.logo-image');
 
 (function printNow() {
   const now = new Date();
@@ -13,3 +25,36 @@ const $headerTimer = document.querySelector(".header-timer");
 
   setTimeout(printNow, 1000);
 })();
+
+const printTitle = (printCount) => {
+
+}
+let titleCount = 0;
+window.onload = e=>{
+  const key = setInterval(()=> {
+    $logoTitle.innerHTML +=title[titleCount];
+    const $newT = $logoTitle.lastElementChild;
+    $newT.style.left = `${titleCount*($newT.textContent===''||4)}%`;
+
+    if(titleCount === titleLength-1) {
+
+      clearInterval(key)
+
+      $logoTitle.style.transition = '2s';
+      $logoTitle.classList.add('midTime');
+      setTimeout(()=>{
+        const $firstT = document.querySelector('.firstT');
+        const $secondT = document.querySelector('.seondT');
+        $logoTitle.classList.add('lastTime');
+        $firstT.style.transition = '2s';
+        $secondT.style.transition = '2s';
+
+      },2000)
+      setTimeout(()=> {
+        $logoImage.classList.add('showImage');
+      },4000)
+    };
+    ++titleCount;
+  },120);
+}
+

c10f9669a75506e05af474799c303578c11d29f3
jungjh123456 <jungjh123456@naver.com>
2020-11-17 11:38:09 +0900

    database

--- public/js/sign.js
+++ public/js/sign.js
@@ -1,12 +1,15 @@
 let user = [];
+const obj = {};
 
 const $signupFieldset = document.querySelector('.signup-fieldset');
 const $signupName = document.querySelector('.signup-name');
 const $signupNick = document.querySelector('.signup-nick');
 const $signupBtn = document.querySelector('.signup-btn');
 const $signupId = document.querySelector('.signup-id');
+const $signupPass = document.querySelector('.signup-pass');
 const $input = document.querySelector('input');
-
+const $signupPass2 = document.querySelector('.signup-pass2');
+const $signupNumber = document.querySelector('.signup-number');
 const redBorder = e => {
   e.target.style.borderBottomColor = 'red';
 
@@ -16,15 +19,28 @@ const redBorder = e => {
 const clearBorder = e => {
   e.target.style.borderBottomColor = '#f0f3f4';
 }
+
 $signupFieldset.onkeyup = e => {
-  if (e.target.matches('.signup-name')){
+
+  if (e.target.matches('.signup-name')) {
     let num = +$signupName.value ? redBorder(e) : e.key > 0 && e.key < 9 ? redBorder(e) : clearBorder(e); 
-    console.log(num)
+    obj['name'] = $signupName.value; 
+  } else if (e.target.matches('.signup-nick')) {
+    obj['ninkname'] = $signupNick.value;
+  } else if (e.target.matches('.signup-id')) {
+    obj['id'] = $signupId.value;
+  } else if (e.target.matches('.signup-pass')) {
+
+  } else if (e.target.matches('.signup-pass2')) {
+    let pass = $signupPass.value !== $signupPass2.value ? redBorder(e) : clearBorder(e);
+    obj['password'] = $signupPass.value;
+  } else if (e.target.matches('.signup-number')) {
+    obj['phoneNumber'] = $signupNumber.value;
   }
 }
 
-function writeUserData(userId, name) {
-  firebase.database().ref('users/' + userId ).set({
+function writeUserData(name) {
+  firebase.database().ref('users/').push({
     username: name
   })
 }
@@ -33,7 +49,7 @@ function writeUserData(userId, name) {
 $signupBtn.onclick = e => {
   e.preventDefault();
   if ($input.value === '') console.log('비었습니다.');
-  writeUserData($signupId.value  , { name: $signupName.value, nickname: $signupNick.value, id: $signupId.value })
+  writeUserData(obj)
 }
 
 

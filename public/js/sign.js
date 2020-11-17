dea58384376dde476e551da9172b0f13b9ac84f5
jungjh123456 <jungjh123456@naver.com>
2020-11-17 10:21:28 +0900

    jjh

--- public/js/sign.js
+++ public/js/sign.js
@@ -1,4 +1,4 @@
-let database = firebase.database();
+let user = [];
 
 const $signupFieldset = document.querySelector('.signup-fieldset');
 const $signupName = document.querySelector('.signup-name');
@@ -7,11 +7,29 @@ const $signupBtn = document.querySelector('.signup-btn');
 const $signupId = document.querySelector('.signup-id');
 const $input = document.querySelector('input');
 
+const redBorder = e => {
+  e.target.style.borderBottomColor = 'red';
+
+  
+}
+
+const clearBorder = e => {
+  e.target.style.borderBottomColor = '#f0f3f4';
+}
+$signupFieldset.onkeyup = e => {
+  if (e.target.matches('.signup-name')){
+    let num = +$signupName.value ? redBorder(e) : e.key > 0 && e.key < 9 ? redBorder(e) : clearBorder(e); 
+    console.log(num)
+  }
+}
+
 function writeUserData(userId, name) {
   firebase.database().ref('users/' + userId ).set({
     username: name
   })
 }
+
+
 $signupBtn.onclick = e => {
   e.preventDefault();
   if ($input.value === '') console.log('비었습니다.');
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ let database = firebase.database();
 
 // db
 
-function writeUserData(userId, name) {
-  firebase.database().ref('users/' + userId ).set({
-    username: name
-  })
+// function writeUserData(userId, name) {
+//   firebase.database().ref('users/' + userId ).set({
+//     username: name
+//   })
   
-}
+// }
 

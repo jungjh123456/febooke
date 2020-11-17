8e4c15717b158ea102220318378d3d34064b8fed
cjy0019 <cjy0029@naver.com>
2020-11-17 20:09:12 +0900

    feat : login

--- public/js/app.js
+++ public/js/app.js
@@ -1,47 +1,27 @@
 let arr = [];
-// --------------------회원가입-------------------------//
+let idArr = [];
+// -------------------- 로그인 -------------------------//
 
-const $loginInput = document.querySelector('.login-input');
-const $passInput = document.querySelector('.pass-input');
-const $btnLogin = document.querySelector('.btn-login');
-
+const $loginInput = document.querySelector(".login-input");
+const $passInput = document.querySelector(".pass-input");
+const $btnLogin = document.querySelector(".btn-login");
 
 const logIn = async () => {
-  const res = await fetch('/users')
+  const res = await fetch("/users");
   arr = await res.json();
+  idArr = arr.map((idArr) => idArr.id);
+  passArr = arr.map((passArr) => passArr.password);
 
-  console.log(arr);
-  $loginI
-}
+  $btnLogin.onclick = (e) => {
+    e.preventDefault();
+    if (idArr.filter((item, i, arr) => $loginInput.value === arr[i]).length) {
+      for (let i = 0; i < passArr.length; i++) {
+        if (passArr[i] === $passInput.value) {
+          window.alert("성공했습니다.");
+        }
+      }
+    } else window.alert("실패했습니다.");
+  };
+};
 
 logIn();
-
-
-
-
-
-// $btnSignup.onclick = e => {
-  
-//   location.assign('../../signup.html');
-// }
-
-// db
-
-
-
-
-
-
-// // storage
-
-// let defaultStorage = firebase.storage();
-// let storageRef = firebase.storage().ref().child('/images/');
-
-
-
-// var file = '../image/ok.png'
-// storageRef.put(file).then(function(snapshot){
-//   console.log(snapshot)
-  
-// })
-

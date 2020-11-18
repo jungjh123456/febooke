8d16684377d59b8399f9062839d8154a633a3182
jungjh123456 <jungjh123456@naver.com>
2020-11-18 21:53:52 +0900

    fix: content add

--- public/js/content.js
+++ public/js/content.js
@@ -16,8 +16,17 @@ window.onload = async () => {
     //     headers: {'Content-Type': 'application/json' },
     //     body: 
     // })
+ 
+
+   const res2 = await fetch(`/comment`);
+    let arr1  = await res2.json();
+    console.log(arr1)
+    render2(arr1);
+
     render3(arr);
     const $commentBtn = document.querySelector('.comment-btn');
+   
+    
     $commentBtn.onclick = async e => {
         const $commenting = document.querySelector('.commenting');
 
@@ -57,12 +66,37 @@ const render = (content) => {
       </div>
       <span class="click-count">${item.clickcount}</span>
     </div>
-    <div class="content">${item.content}</div>`
+    <div class="content">${item.content}</div>
+    <span class="comment-heading">댓글</span>
+    <button class="comment-enrollment">등록순</button>
+    <button class="comment-least">최신순</button>
+    `
 
     })
 
     $container.innerHTML = html;
 }
+
+
+const render2 = (content) => {
+
+    let html = '';
+
+    [...content].forEach(item => {
+
+        html += `<div class="comment">
+        <span>${item.nickname}</span>
+        <div class="comment-list">
+        ${item.commented}
+        </div>
+      </div>`
+
+    })
+
+    $container.innerHTML += html;
+}
+
+
 
 const render3 = content => {
 

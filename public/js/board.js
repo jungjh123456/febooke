1a28488bc37f05e15b3a0b736895b414fef1d27a
jjub0217 <noonssub1004@gmail.com>
2020-11-18 14:15:20 +0900

    delete: variable

--- public/js/board.js
+++ public/js/board.js
@@ -1,31 +1,15 @@
  let arr = [];
 
-  const $years = document.querySelectorAll('.year'); // <- 유사배열객체인 NodeList 가 반환된다.
-  // console.log($years); // NodeList
-  // console.log(typeof $years); // object
-  // console.log(Array.isArray($years)); // false <- 유사배열객체
 
-
-  const $months = document.querySelectorAll('.month');
-  const $dates = document.querySelectorAll('.date');
 
   //글쓰기 버튼
   const $writeBtn = document.querySelector('.write-btn');
- const $boardList = document.querySelector('.board-list');
+  const $boardList = document.querySelector('.board-list');
 
  // login check
-const $loginCheck = document.querySelector('.login-check');
-const $btnGroup = document.querySelector('.btn-group');
+  const $loginCheck = document.querySelector('.login-check');
+  const $btnGroup = document.querySelector('.btn-group');
 
-  const date = new Date();
-  const yearText = date.getFullYear();
-  const monthText = date.getMonth() + 1;
-  const dateText = date.getDate();
-
-
-  $years.forEach(year => { year.textContent =   `${yearText} /` } )
-  $months.forEach(month => { month.textContent =   `${monthText} /` } )
-  $dates.forEach(date => { date.textContent =   dateText } )
 
 
 

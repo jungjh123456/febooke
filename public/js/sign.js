4ab04be432fea8b0539f1af644282fa941b1e43c
cjy0019 <cjy0029@naver.com>
2020-11-19 10:35:19 +0900

    feat : signup

--- public/js/sign.js
+++ public/js/sign.js
@@ -13,6 +13,17 @@ const $input = document.querySelector("input");
 const $signupPass2 = document.querySelector(".signup-pass2");
 const $signupNumber = document.querySelector(".signup-number");
 const $checkPhone = document.querySelector(".check-phone");
+const $checkDuplicated = document.querySelector(".check-duplicated");
+const $checkAlert = document.querySelector(".check-alert");
+const $confirmBtn = document.querySelector(".confirm-btn");
+const $container = document.querySelector(".container");
+const $overlay = document.querySelector(".overlay");
+const $checkText = document.querySelector(".check-text");
+const $checkNoid = document.querySelector(".check-noid");
+const $confirmBtnNoid = document.querySelector(".confirm-btn-noid");
+const $checkEmail = document.querySelector(".check-email");
+const $checkFail = document.querySelector(".check-fail");
+const $confirmBtnFail = document.querySelector(".confirm-btn-fail");
 
 $signupBtn.onclick = async (e) => {
   e.preventDefault();
@@ -124,12 +135,16 @@ const idFilter = async () => {
       console.log("중복");
       state = "false";
       redBorder(e);
+    } else if (!$signupId.value) {
+      $checkEmail.classList.remove("email-on");
+      clearBorder(e);
     } else if (chkEmail($signupId.value) !== true) {
-      console.log("이메일 형식이 아닙니다");
+      $checkEmail.classList.add("email-on");
       state = "false";
       redBorder(e);
     } else {
       state = "true";
+      $checkEmail.classList.remove("email-on");
       clearBorder(e);
     }
 
@@ -142,6 +157,20 @@ const idFilter = async () => {
     }
   };
 };
+
+// id 중복검사
+// $checkDuplicated.onclick = async (e) => {
+//   e.preventDefault();
+//   let res = await (await fetch("/users")).json();
+//   let idArray = res.map((item) => item.id);
+//   if (!$signupId.value) {
+//     window.alert(".");
+//   } else if (idArray.filter((idValue) => $signupId.value === idValue).length) {
+//     console.log("이미 사용중인 아이디입니다.");
+//   } else {
+//     console.log("사용 가능한 아이디입니다.");
+//   }
+// };
 
 idFilter();
 
@@ -202,3 +231,48 @@ if (state === "false") {
   $signupBtn.style.opacity = "1";
   $signupBtn.style.cursor = "pointer";
 }
+
+// alert 창
+$checkDuplicated.onclick = async (e) => {
+  e.preventDefault();
+  let res = await await (await fetch("/users")).json();
+  let idArray = res.map((item) => item.id);
+  if (!$signupId.value) {
+    $overlay.classList.toggle("overlay-noid");
+    $checkNoid.classList.toggle("check-noid-on");
+  } else if (
+    idArray.filter((idValue) => $signupId.value === idValue).length ||
+    chkEmail($signupId.value) !== true
+  ) {
+    $checkFail.classList.toggle("check-fail-on");
+    $overlay.classList.toggle("overlay-fail");
+  } else {
+    $checkAlert.classList.toggle("check-on");
+    $overlay.classList.toggle("overlay-active");
+    $checkText.textContent = "Available!";
+  }
+};
+
+$confirmBtn.onclick = (e) => {
+  e.preventDefault();
+  $checkAlert.classList.toggle("check-on");
+  $overlay.classList.toggle("overlay-active");
+};
+
+$confirmBtnNoid.onclick = (e) => {
+  e.preventDefault();
+  $checkNoid.classList.toggle("check-noid-on");
+  $overlay.classList.toggle("overlay-noid");
+};
+
+$confirmBtnFail.onclick = (e) => {
+  e.preventDefault();
+  $checkFail.classList.toggle("check-fail-on");
+  $overlay.classList.toggle("overlay-fail");
+};
+
+window.onkeyup = (e) => {
+  if (e.keyCode !== 27) return;
+  $checkAlert.classList.remove("check-on");
+  $overlay.classList.remove("overlay-active");
+};

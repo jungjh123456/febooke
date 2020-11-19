37f20114feb9941c788325b9e62455142be80d64
cjy0019 <cjy0029@naver.com>
2020-11-19 21:14:15 +0900

    feat find

--- public/js/app.js
+++ public/js/app.js
@@ -37,6 +37,7 @@ const logIn = async () => {
 
   $btnLogin.onclick = async (e) => {
     e.preventDefault();
+
     // 아이디 성공
     if (idArr.filter((item, i, arr) => $loginInput.value === arr[i]).length) {
       for (let i = 0; i < passArr.length; i++) {
